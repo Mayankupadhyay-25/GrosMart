5031febebf30da18aec4440c6808a3a5f3d776c9
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-09-08 00:35:37 +0530

    searchbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import { GoHeartFill } from "react-icons/go";
+import { HiShoppingBag } from "react-icons/hi2";
+import { IoSearchSharp } from "react-icons/io5";
+
+
 const Navbar = () => {
   return (
     <header className='bg-white'>
@@ -27,9 +31,22 @@ const Navbar = () => {
             </ul>
 
             {/* Nav Action */}
-            <div>
-                <a href="#">
+            <div className='flex items-center gap-x-5'>
+
+                {/* input field */}
+                <div className='flex p-1 border-2 border-orange-500 rounded-full'  >
+                    <input type="text" nmae="text" placeholder='Search' autoComplete='off' 
+                    className='flex-1 h-[5vh] px-3 focus:outline-none'/>
+                    <button className='bg-gradient-to-b from-red-600 to-orange-400 text-white w-10 h-10  flex justify-center items-center rounded-full text-xl '>
+                        <IoSearchSharp />
+                    </button>
+                </div>
+                <a href="#" className='taxt-zinc-800 text-2xl'>
                     <GoHeartFill />
+                </a>
+                 <a href="#" className='taxt-zinc-800 text-2xl'>
+                    <HiShoppingBag />
+
                 </a>
             </div>
         </nav>
