b0d399b714baa5250438e5785b347da56e3734bc
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-09-08 02:51:44 +0530

    text

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,19 +2,20 @@ import React from 'react'
 import { GoHeartFill } from "react-icons/go";
 import { HiShoppingBag } from "react-icons/hi2";
 import { IoSearchSharp } from "react-icons/io5";
+import { TbMenu2 } from "react-icons/tb";
 
 
 const Navbar = () => {
   return (
-    <header className='bg-white'>
-         <nav className="max-w-[1400px] mx-auto h-[14vh] justify-between items-center px-10 flex justify-between ">
+    <header className='bg-white fixed top-0 right-0 left-0'>
+         <nav className="max-w-[1400px] mx-auto md:h-[14vh] h-[12vh] items-center px-10 flex justify-between ">
             {/* Logo */}
             <a href="#" className="text-3xl font-bold ">
             Gr<span className='text-orange-500 uppercase'>o</span>sMart
             </a>
 
             {/* Desktop Menu */}
-            <ul className="flex items-center gap-x-18">
+            <ul className="md:flex   items-center gap-x-18 hidden">
                 <li>
                     <a href="#" className='font-semibold tracking-wider text-orange-500'>Home</a>
             </li>
@@ -34,7 +35,7 @@ const Navbar = () => {
             <div className='flex items-center gap-x-5'>
 
                 {/* input field */}
-                <div className='flex p-1 border-2 border-orange-500 rounded-full'  >
+                <div className='md:flex p-1 border-2 border-orange-500 rounded-full hidden'  >
                     <input type="text" nmae="text" placeholder='Search' autoComplete='off' 
                     className='flex-1 h-[5vh] px-3 focus:outline-none'/>
                     <button className='bg-gradient-to-b from-red-600 to-orange-400 text-white w-10 h-10  flex justify-center items-center rounded-full text-xl '>
@@ -46,7 +47,10 @@ const Navbar = () => {
                 </a>
                  <a href="#" className='taxt-zinc-800 text-2xl'>
                     <HiShoppingBag />
-
+                </a>
+                {/* <Hamburguer>  */}
+                <a href="#" className='taxt-zinc-800 text-3xl md:hidden '>
+                    <TbMenu2 />
                 </a>
             </div>
         </nav>
