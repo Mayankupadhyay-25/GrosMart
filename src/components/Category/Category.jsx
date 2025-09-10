d6cd9a51debc1c0a6eaae8d24ee1bc6f74ccdf7e
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-09-10 16:30:56 +0530

    category

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
+import Heading from '../Heading/Heading'
 
 const Category = () => {
   return (
     <section>
         <div className='py-20'>
-            <div className='w-fit mx-auto'>
-                <h2 className='md:text-5xl text-[2.5rem] font-bold'><span className='text-orange-500'>Show</span> by Category
-                </h2>
-                <div className='w-35 h-1 bg-orange-300 mt-6 ml-auto  '>
-
-                </div>
-            </div> 
-            
+            <Heading highlight="Shop" heading="by Category"/>
         </div>
 
     </section>
@@ -19,3 +13,4 @@ const Category = () => {
 }
 
 export default Category
+
