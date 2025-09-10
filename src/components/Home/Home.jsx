f53d36abd89ba1954adbd0a2f29cde0b15bf8c24
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-09-10 16:03:04 +0530

    settitle

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar.jsx'
 import Hero from '../Hero/Hero.jsx'
+import Category from '../Category/category.jsx'
 const Home = () => {
   return (
     <div>
         <Navbar />
         <Hero />
+        <Category />
     </div>
   )
 }
