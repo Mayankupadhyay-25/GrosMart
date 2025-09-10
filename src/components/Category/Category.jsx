429c3913cde825999e319d0de9bba1fd49785782
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-09-10 16:02:48 +0530

    Title

---
+++ src/components/Category/Category.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Category = () => {
+  return (
+    <section>
+        <div className='py-20'>
+            <div className='w-fit mx-auto'>
+                <h2 className='md:text-5xl text-[2.5rem] font-bold'><span className='text-orange-500'>Show</span> by Category
+                </h2>
+                <div className='w-35 h-1 bg-orange-300 mt-6 ml-auto  '>
+
+                </div>
+            </div> 
+            
+        </div>
+
+    </section>
+  )
+}
+
+export default Category
