b518f52921c5569ec10cc71190d9e15292f2bdfa
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-09-18 23:43:38 +0530

    Products section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from '../Navbar/Navbar.jsx'
 import Hero from '../Hero/Hero.jsx'
 import Category from '../Category/category.jsx'
 import Values from '../Values/Values.jsx'
+import Products from '../Products/Products.jsx'
 const Home = () => {
   return (
     <div>
@@ -10,6 +11,7 @@ const Home = () => {
         <Hero />
         <Category />
         <Values/>
+        <Products/>
     </div>
   )
 }
