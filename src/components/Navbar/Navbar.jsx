ffd25d1341dc1fd9a2fe47a50ee5172f40342f6a
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2025-09-21 02:47:22 +0530

    responsivness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -30,9 +30,9 @@ const Navbar = () => {
     <header className= {`bg-white fixed top-0 right-0 left-0 z-50 ${isScrolled ? 'shadow-lg' : ''} `}>
          <nav className="max-w-[1400px] mx-auto md:h-[14vh] h-[12vh] items-center px-10 flex justify-between ">
             {/* Logo */}
-            <a href="#" className="text-3xl font-bold ">
+          <a href="#" className="text-3xl font-bold ">
             Gr<span className='text-orange-500 uppercase'>o</span>sMart
-            </a>
+            </a>  
 
             {/* Desktop Menu */}
             <ul className="md:flex   items-center gap-x-18 hidden">
---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Heading from '../Heading/Heading'
+
+const Testimonials = () => {
+  return (
+    <section>
+        <div className='max-w-[1400px] px-10 mx-auto py-10'>
+            <Heading highlight="Customers" heading=" saying"/>
+        </div>
+    </section>
+  )
+}
+
+export default Testimonials
